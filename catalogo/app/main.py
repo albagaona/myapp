57ac9ee8f53c3523e73ba8ce46f888ac2a525d53
Alba Gaona Arevalo <alba.1497@gmail.com>
2021-01-13 15:49:39 +0000

    Clase 14-12

--- catalogo/app/main.py
+++ catalogo/app/main.py
@@ -1,43 +1,7 @@
-from flask import jsonify, request, Flask, make_response
-from catalog import get_products, create_product, get_product
-from flask_cors import CORS
-import os
-import redis
-
+from flask import jsonify, request, Flask
+from catalog import get_products, create_product
 
 app = Flask(__name__)
-CORS(app)
-
-redis_host = os.environ.get('REDIS_HOST', None)
-redis_port = os.environ.get('REDIS_PORT', None)
-
-if redis_host and redis_port:
-    redis_client = redis.Redis(
-        host=os.getenv('REDIS_HOST'),
-        port=os.getenv('REDIS_PORT'),
-        db=0, decode_responses=True
-    )
-else:
-    redis_client = None
-
-
-@app.route('/product/<sku>', methods=['GET', ])
-def get_product_by_sku(sku):
-    product = redis_client.hgetall(sku) if redis_client else None
-    if not product:
-        product = get_product(sku)
-        if not product:
-            return make_response(jsonify({
-                "error": f"SKU {sku} not found!"
-            }), 404)
-        product['cache'] = 'miss'
-        if redis_client:
-            redis_client.hmset(product['sku'], product)
-    else:
-        pass
-        product['cache'] = 'hit'
-
-    return jsonify(product)
 
 
 @app.route('/product', methods=['GET', 'POST'])
@@ -50,12 +14,12 @@ def list_all_products():
 
     if request.method == 'POST':
         data = request.get_json()
-        new_sku = create_product(
-            None,
+        create_product(
+            data['sku'],
             data['title'],
             data['long_description'],
             data['price_euro'])
-        return jsonify({"status": "ok", "sku": new_sku})
+        return jsonify({"status": "ok"})
 
 
 @app.route('/hello')
